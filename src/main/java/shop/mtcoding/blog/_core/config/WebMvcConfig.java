e69f784a95930b7b1c0eac7e95e7eba61c4caf1b
coderyu5523 <coderyu5523@naver.com>
2024-03-20 12:07:17 +0900

    핸들러 수정

--- src/main/java/shop/mtcoding/blog/_core/config/WebMvcConfig.java
+++ src/main/java/shop/mtcoding/blog/_core/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ package shop.mtcoding.blog._core.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import shop.mtcoding.blog._core.interceptor.ForbiddenInterceptor;
 import shop.mtcoding.blog._core.interceptor.LoginInterceptor;
 
 @Configuration // ioc 등록
--- src/main/java/shop/mtcoding/blog/_core/err/MyExeptionHandler.java
+++ src/main/java/shop/mtcoding/blog/_core/err/MyExeptionHandler.java
@@ -1,40 +1,45 @@
 package shop.mtcoding.blog._core.err;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import shop.mtcoding.blog._core.err.exception.*;
+import shop.mtcoding.blog._core.util.ApiUtil;
 
-@ControllerAdvice //runtimeException 이 터지만 해당 파일로 오류가 모인다.
+@RestController //runtimeException 이 터지만 해당 파일로 오류가 모인다.
 public class MyExeptionHandler{
 
     @ExceptionHandler(Exception400.class)
-    public String ex400(RuntimeException e, HttpServletRequest request){
-        request.setAttribute("msg",e.getMessage());
+    public ResponseEntity<?> ex400(RuntimeException e){
 
-        return "err/400";
+        ApiUtil<?> apiUtil = new ApiUtil<>(400,e.getMessage()); // http body 내부의 구성한 객체
+        return new ResponseEntity<>(apiUtil, HttpStatus.BAD_REQUEST); // http header , http body
     }
     @ExceptionHandler(Exception401.class)
-    public String ex401(RuntimeException e, HttpServletRequest request){
-        request.setAttribute("msg",e.getMessage());
-        return "err/401";
+    public ResponseEntity<?> ex401(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(401,e.getMessage()); // http body 내부의 구성한 객체
+        return new ResponseEntity<>(apiUtil,HttpStatus.UNAUTHORIZED);
     }
-    @ExceptionHandler(Exception403.class)
-    public String ex403(RuntimeException e,HttpServletRequest request){
-        request.setAttribute("msg",e.getMessage());
 
-        return "err/403";
+    @ExceptionHandler(Exception403.class)
+    public ResponseEntity<?> ex403(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(403,e.getMessage()); // http body 내부의 구성한 객체
+        return new ResponseEntity<>(apiUtil,HttpStatus.FORBIDDEN);
     }
     @ExceptionHandler(Exception404.class)
-    public String ex404(RuntimeException e,HttpServletRequest request){
-        request.setAttribute("msg",e.getMessage());
+    public ResponseEntity<?> ex404(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(404,e.getMessage()); // http body 내부의 구성한 객체
 
-        return "err/404";
+        return new ResponseEntity<>(apiUtil,HttpStatus.NOT_FOUND);
     }
+
     @ExceptionHandler(Exception500.class)
-    public String ex500(RuntimeException e,HttpServletRequest request){
-        request.setAttribute("msg",e.getMessage());
-        return "err/500";
+    public ResponseEntity<?> ex500(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(500,e.getMessage()); // http body 내부의 구성한 객체
+        return new ResponseEntity<>(apiUtil,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 
